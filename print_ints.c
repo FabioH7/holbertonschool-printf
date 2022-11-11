e5e88a16fb088bd38ede047210a59b35e77bcb55
FabioH7 <fhysollari4@gmail.com>
2022-11-11 03:44:34 -0800

    printf completed

--- print_ints.c
+++ print_ints.c
@@ -1,14 +1,24 @@
 #include "main.h"
 
+/**
+ * change_int - take the value of int
+ * @arg_list: argument to change
+ * Return: function to print int a.k.a len
+ */
 int change_int(va_list arg_list)
 {
-	long int x = va_arg(arg_list, int);
-	
+	long int x = va_arg(arg_list, int);	
 	if (x < 0)
 		return (print_int(x, 2));
 	return (print_int(x, 1));
 }
 
+/**
+ * print_int - print int
+ * @n: number to print
+ * @count: len of int
+ * Return: new_count
+ */
 int print_int(long int n, int count)
 {
 	int new_count = count;
